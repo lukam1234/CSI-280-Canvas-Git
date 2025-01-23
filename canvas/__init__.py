b425d0ed38eda6da200e36294510e785d46487b0
EOF-D <END-OFD@pm.me>
2025-01-23 15:22:15 -0500

    feat: initial commit

--- canvas/__init__.py
+++ canvas/__init__.py
@@ -1,4 +1,6 @@
 from __future__ import annotations
 
 __version__ = "v0.1.0"
-__authors__ = ["Andy Zheng <END-OFD@pm.me>", ] # TODO: Add team members.
+__authors__ = [
+    "Andy Zheng <END-OFD@pm.me>",
+]  # TODO: Add team members.
--- canvas/__main__.py
+++ canvas/__main__.py
@@ -1,6 +1,7 @@
 from . import __version__
 from argparse import ArgumentParser
 
+
 def gen_parser() -> ArgumentParser:
     """Creates the argument parser for the CLI.
     This is how we'll interact with the Canvas LMS API.
@@ -11,13 +12,11 @@ def gen_parser() -> ArgumentParser:
 
     parser = ArgumentParser(prog="canvas")
     parser.add_argument(
-        "-v",
-        "--version",
-        action="version",
-        version=f"Printer: {__version__}"
+        "-v", "--version", action="version", version=f"Printer: {__version__}"
     )
 
     return parser
+
 
 def main() -> None:
     parser = gen_parser()
@@ -25,6 +24,7 @@ def main() -> None:
 
     if not vars(args):
         parser.print_help()
+
 
 if __name__ == "__main__":
     main()
